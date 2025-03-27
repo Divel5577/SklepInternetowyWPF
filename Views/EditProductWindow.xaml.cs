9589b6e9e33c5622b3f184ebd055abab4a49a0e4
Divel5577 <gitpdulkowski@gmail.com>
2025-03-28 00:03:04 +0100

    implement full stock quantity management for products

--- Views/EditProductWindow.xaml.cs
+++ Views/EditProductWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
 using SklepInternetowyWPF.Models;
 using SklepInternetowyWPF.ViewModels;
 
@@ -12,6 +13,10 @@ namespace SklepInternetowyWPF.Views
         {
             InitializeComponent();
             Product = product;
+
+            if (Product.StockMax < Product.Stock)
+                Product.StockMax = Product.Stock;
+
             DataContext = Product;
 
             var vm = new ProductViewModel();
@@ -20,8 +25,34 @@ namespace SklepInternetowyWPF.Views
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            if (Product.Price < 0)
+            {
+                MessageBox.Show("Cena nie może być ujemna.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (Product.Stock < 0)
+            {
+                MessageBox.Show("Stan magazynowy nie może być ujemny.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (Product.Stock % 1 != 0)
+            {
+                MessageBox.Show("Stan magazynowy musi być liczbą całkowitą.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            Product.StockMax = Product.Stock;
+
+
             DialogResult = true;
             Close();
+        }
+
+        private void StockBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"^\d+$");
         }
     }
 }
