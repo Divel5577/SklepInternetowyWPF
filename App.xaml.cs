e1c1dc5468da9a6346be59b2460505e49d771264
Divel5577 <gitpdulkowski@gmail.com>
2025-06-12 23:32:59 +0200

    added app icon

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using SklepInternetowyWPF.Views;
+﻿using IWshRuntimeLibrary;
+using SklepInternetowyWPF.Views;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
